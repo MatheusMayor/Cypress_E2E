19e773db0105ba66a33e15550d9cf65b95b901fa
camilapessoa <camilabentespessoa@gmail.com>
2024-03-01 18:15:45 -0300

    video 2.3 - Testando pagina de login com boas praticas

--- cypress/e2e/login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Página de login', () => {
-    it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
-      cy.visit('https://adopet-frontend-cypress.vercel.app/');
-      cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="input-loginEmail"]').type('ana@email.com');
-      cy.get('[data-test="input-loginPassword"]').type('Senha123');
-      cy.get('[data-test="submit-button"]').click();
-    })
-})
